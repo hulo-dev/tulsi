e2843a63f029f429a434365a54f707bc9a00e9e4
hulo-dev <hello@hulo.dev>
2024-07-05 19:51:21 +0300

    home rest sections

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+//tabs
 function changeTab(tab) {
     const tabContents = document.querySelectorAll('.tab-content');
     tabContents.forEach(content => {
@@ -32,3 +33,60 @@ document.addEventListener('DOMContentLoaded', function () {
     setActiveLink(document.querySelector('.tab-button[data-tab="1"]'));
 });
 
+
+//set left margin to section with outside image
+
+/*function updateMargin() {
+    const outsideImageSection = document.querySelector('.outside-image-section');
+
+    let closestSection = outsideImageSection.previousElementSibling;
+   
+    if (!closestSection || closestSection.tagName !== 'SECTION') {
+        closestSection = outsideImageSection.nextElementSibling;
+    }
+
+    if (closestSection && closestSection.tagName === 'SECTION') {
+        const parentContentWrapper = closestSection.querySelector('.content-wrapper');
+        if (parentContentWrapper) {
+            const parentMarginLeft = window.getComputedStyle(parentContentWrapper).marginLeft;
+            const childContentWrapper = outsideImageSection.querySelector('.content-wrapper');
+
+            if (childContentWrapper) {
+                childContentWrapper.style.marginLeft = parentMarginLeft;
+            } 
+        } 
+    } 
+}
+
+window.addEventListener('DOMContentLoaded', updateMargin);
+window.addEventListener('resize', updateMargin);*/
+
+
+function updateMargin() {
+    const leftMarginSections = document.querySelectorAll('.cs-left-margin');
+
+    leftMarginSections.forEach(leftMarginSection => {
+        let closestSection = leftMarginSection.previousElementSibling;
+
+        if (!closestSection || closestSection.tagName !== 'SECTION') {
+            closestSection = leftMarginSection.nextElementSibling;
+        }
+
+        if (closestSection && closestSection.tagName === 'SECTION') {
+            const parentContentWrapper = closestSection.querySelector('.content-wrapper');
+            if (parentContentWrapper) {
+                const parentMarginLeft = window.getComputedStyle(parentContentWrapper).marginLeft;
+                //const childContentWrapper = leftMarginSection.querySelector('.content-wrapper');
+                const childContentWrapper = leftMarginSection.querySelector('.content .text-wrapper');
+
+                if (childContentWrapper) {
+                    //childContentWrapper.style.marginLeft = parentMarginLeft;
+                    childContentWrapper.style.paddingLeft = parentMarginLeft;
+                } 
+            } 
+        }
+    });
+}
+
+window.addEventListener('DOMContentLoaded', updateMargin);
+window.addEventListener('resize', updateMargin);
